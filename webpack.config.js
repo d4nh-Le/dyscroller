d4221bc8c0b160f5bbd4c43526a9c6c2bfaa2492
d4nh-Le <d4nhle02@gmail.com>
2024-08-09 18:58:42 -0600

    Project initialized, structure setup using React, Node and Webpack

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: './src/app.jsx',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+  },
+  module: {
+    rules: [{
+      test: /\.(js|jsx)$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-react', '@babel/preset-env'],
+        },
+      },
+    }],
+  },
+  plugins: [new HtmlWebpackPlugin({
+    template: './src/app.html',
+    filename: 'app.html',
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: 'public'}
+      ],
+    }),
+  ]
+};
