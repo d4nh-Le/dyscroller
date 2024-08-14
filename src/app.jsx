35d99cc3f93b2c54acc17890b919682ef294214d
Nhat Trinh <nhatmtrinh95@gmail.com>
2024-08-13 19:18:07 -0700

    add apis to save, delete, and get saved domains

--- src/app.jsx
+++ src/app.jsx
@@ -1,14 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { createRoot } from "react-dom/client";
 
+import {
+    isChromeExtension, onUrlChange, isUrlSaved,
+} from "./utils";
 
 const App = () => {
+    useEffect(() => {
+        if (isChromeExtension()) {
+            onUrlChange((url) => {
+                isUrlSaved(url).then((isSaved) => {
+                    if (isUrlSaved(url)) {
+                        console.log("Watching for doomscrolling!");
+                    }
+                });
+            });
+        }
+    }, []);
+
     return (
         <div>
-        <h1>Start Dyscroller here!</h1>
+            <h1>Start Dyscroller here!</h1>
         </div> 
     );
-    };
+};
 
 const root = createRoot(document.getElementById("root"));
 root.render(<App />);
---
+++ src/utils.js
@@ -0,0 +1,88 @@
+
+export const saveUrl = (inputUrl) => {
+  const targetUrl = getFullUrl(inputUrl);
+  if (targetUrl) {
+    chrome.storage.local.get(
+      {savedUrls: []},
+      (result) => {
+        const { savedUrls } = result;
+        const urls = [...savedUrls];
+
+        if (!urls.includes(targetUrl)) {
+            urls.push(targetUrl);
+            chrome.storage.local.set(
+              {savedUrls: urls},
+              () => console.log(targetUrl + " saved.")
+            );
+        }
+      }
+    );
+  }
+}
+
+
+export const getUrls = () => {
+  return new Promise((resolve) => {
+    chrome.storage.local.get("urls", (result) => {
+      resolve(result.urls || []);
+    });
+  });
+};
+
+export const removeUrl = (targetUrl) => {
+  chrome.storage.local.get(
+    {savedUrls: []},
+    (result) => {
+      const { savedUrls } = result;
+      const urls = [...savedUrls];
+      const finalUrls = urls.filter((savedUrl) => savedUrl !== targetUrl);
+
+      chrome.storage.local.set(
+        {savedUrls: finalUrls},
+        () => {
+          console.log(url + " has been removed.");
+        }
+      );
+  });
+}
+
+export const isChromeExtension = () => {
+  return typeof chrome !== "undefined";
+};
+
+export const onUrlChange = (callback) => {
+  chrome.webNavigation.onHistoryStateUpdated.addListener((historyState) => {
+    const { url } = historyState;
+    if (typeof callback === "function") {
+      callback(url);
+    }
+  });
+};
+
+export const isUrlSaved = (url, callback) => {
+  chrome.storage.local.get({savedUrls: []}, function(result) {
+    const { savedUrls } = result;
+    const isSaved = savedUrls.includes(url);
+    callback(isSaved);
+  });
+};
+
+export const getFullUrl = (input) => {
+  // Trim any leading or trailing whitespace
+  let inputUrl = input;
+  inputUrl = inputUrl.trim();
+
+  // Check if the input already starts with a valid protocol
+  if (!inputUrl.startsWith("http://") && !inputUrl.startsWith("https://")) {
+      // If not, prepend "https://" by default
+      inputUrl = "https://" + inputUrl;
+  }
+
+  try {
+      let url = new URL(inputUrl);
+      return url.href;
+  } catch (e) {
+      console.error("Invalid URL provided: " + input);
+      return null;
+  }
+};
