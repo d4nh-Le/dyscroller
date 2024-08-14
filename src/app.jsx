e9680925b483b58c59f57590224d2591a9200a8d
Benson Tran <lolbensontheburrito@gmail.com>
2024-08-13 23:00:48 -0700

    Revert "Reverting"
    This reverts commit 2bc602f0d7be37737fbc8d935b0dd56f00a2e514.

--- src/app.jsx
+++ src/app.jsx
@@ -1,18 +1,14 @@
-// src/App.js
-import React from 'react';
-import PopupContainer from './components/PopupContainer';
-import Navbar from './components/Navbar';
-import SearchContainer from './components/SearchContainer';
-import TasksContainer from './components/TasksContainer';
-import './App.css'; // Import any global CSS
+import React from "react";
+import { createRoot } from 'react-dom/client';
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <PopupContainer />
-    </div>
-  );
-}
 
-export default App;
+const App = () => {
+    return (
+        <div>
+        <h1>Start Dyscroller here!</h1>
+        </div> 
+    );
+    };
+
+const root = createRoot(document.getElementById('root'));
+root.render(<App />);
