30b62dcd8a828b692f56f075d6a5f6b90c434e96
Nhat Trinh <nhatmtrinh95@gmail.com>
2024-08-16 02:26:20 -0700

    fix generic apis

--- src/utils.js
+++ src/utils.js
@@ -1,23 +1,22 @@
 
 export const saveData = (key, value) => {
   chrome.storage.local.get(
-    { savedData: {} },
+    { [key]: [] },
     (result) => {
-      const { savedData } = result;
-      const data = { ...savedData };
+      const newData = [...result[key]];
 
-      if (key && value) {
-        if (!data.hasOwnProperty(key)) {
-          data[key] = value;
+      if (value) {
+        if (!newData.includes(key)) {
+          newData.push(value);
           chrome.storage.local.set(
-            { savedData: data },
-            () => alert(`${key}: ${value} saved.`)
+            { [key]: newData },
+            () => alert(`Value for ${key} saved.`)
           );
         } else {
-          alert(`${key}: ${value} is already saved.`);
+          alert(`Value for ${key} already saved.`);
         }
       } else {
-        alert("Invalid key or value provided.");
+        alert("Invalid value provided.");
       }
     }
   );
@@ -26,29 +25,22 @@ export const saveData = (key, value) => {
 
 export const getData = (key) => {
   return new Promise((resolve) => {
-    chrome.storage.local.get({ savedData: {} }, (result) => {
+    chrome.storage.local.get({ [key]: [] }, (result) => {
       resolve(result[key]);
     });
   });
 };
 export const removeData = (key, value) => {
   chrome.storage.local.get(
-    { savedData: {} },
+    { [key]: [] },
     (result) => {
-      const { savedData } = result;
-      const data = { ...savedData };
-      const newData = {};
-
-      Object.keys(data).forEach((dataKey) => {
-        if (data[dataKey] !== value) {
-          newData[dataKey] = data[dataKey];
-        }
-      });
+      const data = [ ...result[key] ];
+      const newData = data.filter((item) => item !== value);
 
       chrome.storage.local.set(
-        { savedData: newData },
+        { [key]: newData },
         () => {
-          console.log(`${key}: ${value} has been removed.`);
+          console.log(`Value has been removed from ${key}.`);
         }
       );
     }
@@ -70,26 +62,11 @@ export const onUrlChange = (callback) => {
 
 export const isDataSaved = (key, value, callback) => {
   chrome.storage.local.get(
-    { savedData: {} },
+    { [key]: [] },
     (result) => {
-      const { savedData } = result;
-      const data = { ...savedData };
-
-      if (key && value) {
-        if (!data.hasOwnProperty(key)) {
-          data[key] = value;
-          chrome.storage.local.set(
-            { savedData: data },
-            () => {
-              callback(true);
-            }
-          );
-        } else {
-          callback(false);
-        }
-      } else {
-        callback(false);
-      }
+      const data = [ ...result[key] ];
+      const isSaved = data.includes(value);
+      callback(isSaved);
     }
   );
 };
